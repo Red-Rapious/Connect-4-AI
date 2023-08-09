487cdcf30f37fa3b7b45424d10836a60569aaa19
Red Rapious <red.rapious@gmail.com>
2023-08-09 17:37:37 +0200

    Added position notation and started benchmark lib

--- libs/benchmark/src/lib.rs
+++ libs/benchmark/src/lib.rs
@@ -1,14 +1,80 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+use lib_game_board::SequencePosition;
+
+pub struct Benchmark {
+    test_sets: Vec<TestSet>
+}
+
+impl Benchmark {
+    pub fn new() -> Self {
+        Self { test_sets: vec![] }
+    }
+}
+
+struct TestSet {
+    length: usize,
+    rating: usize,
+    games_moves: Vec<(SequencePosition, i32)>
+}
+
+impl TestSet {
+    pub fn new(length: usize, rating: usize) -> Self {
+        assert!(1 <= rating && rating <= 3);
+        assert!(1 <= length && length <= 3);
+
+        let games_moves = TestSet::load_test(length, rating);
+
+        Self { length, rating, games_moves }
+    }
+
+    fn load_test(length: usize, rating: usize) -> Vec<(SequencePosition, i32)> {
+        let file_path = format!("./datasets/Test_L{}_R{}", length, rating);
+        let file = File::open(&file_path).expect(format!("Unable to read file: {}", file_path).as_str());
+        let reader = BufReader::new(file);
+        let mut games_moves = Vec::with_capacity(1_000);
+
+        for line in reader.lines() {
+            let line_content = line.expect("Unable to read line");
+            let elements: Vec<String> = line_content
+                .split_whitespace()
+                .map(str::parse)
+                .collect::<Result<_, _>>()
+                .expect("Unable to parse line.");
+
+            assert_eq!(elements.len(), 2);
+
+            let position = SequencePosition::from(&elements[0]);
+            let score: i32 = elements[1].parse().expect("Unable to parse score.");
+
+            games_moves.push((position, score));
+        }
+        
+        games_moves
+    }
+
+    pub fn games_moves(&self) -> &Vec<(SequencePosition, i32)> {
+        &self.games_moves
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    mod load {
+        use super::*;
+
+        #[test]
+        #[allow(non_snake_case)]
+        pub fn load_L1_R1() {
+            let test_set = TestSet::new(1, 1);
+            
+            assert_eq!(
+                test_set.games_moves()[0],
+                (SequencePosition::from(&"32164625".to_string()), 11)
+            )
+        }
     }
 }
